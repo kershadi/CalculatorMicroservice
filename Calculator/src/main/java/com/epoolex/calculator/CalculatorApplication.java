1e72ccc63403da5d3c1827b1f77395327c8ef90c
kershadi <ke47@yahoo.com>
2018-03-04 23:51:23 -0600

    First check in for calculator project

---
+++ Calculator/src/main/java/com/epoolex/calculator/CalculatorApplication.java
@@ -0,0 +1,14 @@
+package com.epoolex.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan (basePackages = {"com.epoolex.controller"})
+public class CalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorApplication.class, args);
+	}
+}
